04e1de957009fed6fcc5247af82e8b613fd7e74f
vasanthakumar.kalidas <vasanthakumar.kalidas@gmail.com>
2025-08-04 07:14:51 +0000

    first commit

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
-
-const API_BASE_URL = import.meta.env.REACT_APP_API_URL || 'http://localhost:5000'
-  
+const API_BASE_URL =
+  import.meta.env.VITE_APP_API_URL ||
+  "https://localhost:5000";
 
 export default API_BASE_URL;
