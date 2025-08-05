6c019ab803809d8eea17f8b2222a76910683fbe9
vasanthakumar.kalidas <vasanthakumar.kalidas@gmail.com>
2025-08-05 06:15:58 +0000

    small change

--- src/form/createJob.jsx
+++ src/form/createJob.jsx
@@ -41,7 +41,7 @@ const CreateJob = () => {
       onSubmit={handleSubmit(onSubmit)}
       className="z-50 form-container max-w-4xl mx-auto p-6 bg-white shadow-md rounded-md"
     >
-      <h1 className="text-2xl font-bold text-center mb-6">
+      <h1 className="text-2xl font-semibold text-center mb-6">
         Create Job Opening
       </h1>
 
